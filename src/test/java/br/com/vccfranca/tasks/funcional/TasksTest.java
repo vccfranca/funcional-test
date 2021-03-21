d94b1025e0214c62bbd63c73d51a5cd69e07eca3
Victor C. C. França <you@example.com>
2021-03-21 10:58:35 -0300

    add healthcheck

--- src/test/java/br/com/vccfranca/tasks/funcional/TasksTest.java
+++ src/test/java/br/com/vccfranca/tasks/funcional/TasksTest.java
@@ -17,9 +17,9 @@ public class TasksTest {
 	public WebDriver acessarAplicacao() throws MalformedURLException {
 //		WebDriver driver = new ChromeDriver();
 		DesiredCapabilities cap = DesiredCapabilities.chrome();
-		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.0.121:4444/wd/hub"), cap );
-//		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.0.163:4444/wd/hub"), cap );
-		driver.navigate().to("http://192.168.0.121:8085/tasks");
+//		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.0.121:4444/wd/hub"), cap );
+		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.0.163:4444/wd/hub"), cap );
+		driver.navigate().to("http://192.168.0.121:8003/tasks");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		return driver;
 	}
